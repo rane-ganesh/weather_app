158ea69b58229f44fbee80ccc0983dab006ffb20
rane-ganesh <raneganesh888@gmail.com>
2024-03-22 23:30:08 +0530

    App body done

---
+++ lib/hourly_forecast_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HourlyForecastItem extends StatelessWidget {
+  final String time;
+  final IconData icon;
+  final String temprature;
+  const HourlyForecastItem({
+    super.key,
+    required this.time,
+    required this.icon,
+    required this.temprature,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      child: Container(
+        width: 100,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              time,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Icon(
+              icon,
+              size: 32,
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              temprature,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
